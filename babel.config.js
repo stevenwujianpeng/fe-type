c4f3e6a7899d3b89d834604b16c603f5caca0c4b
吴建鹏 <jianpeng.wu@geely.com>
2018-11-01 20:26:19 +0800

    重写babel的配置文件；重新构建发布包；

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,18 @@
-const presets = [
-  [
-    "@babel/env", {
-      "modules": false
-    }
-  ],
-];
+// const presets = ["@babel/preset-env"];
+module.exports = function (api) {
+  const env = api.env();
+  const presets = [["@babel/preset-env"]];
+  const plugins = [];
 
-module.exports = { presets };
+  if (env === 'production') {
+    presets[0].push({
+      "targets": "> 0.25%, not dead",
+      "modules": false
+    });
+  }
+
+  return {
+    presets,
+    plugins
+  }
+};
