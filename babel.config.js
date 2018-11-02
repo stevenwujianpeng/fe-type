374281a81c86dda7d5362c0d02a6ed378694c5f3
吴建鹏 <jianpeng.wu@geely.com>
2018-11-02 17:01:01 +0800

    修复了打包的问题；

--- babel.config.js
+++ babel.config.js
@@ -6,13 +6,13 @@ module.exports = function (api) { // eslint-disable-line
 
   if (env === 'production') {
     presets[0].push({
-      // "targets": "last 2 versions, > 1%, ie >= 9, Android >= 4.4, iOS >= 8",
+      "targets": "last 2 version",
       "modules": false
     });
     plugins.push([
       "@babel/plugin-transform-runtime",
       {
-        "corejs": 2,
+        "corejs": false,
         "helpers": true,
         "regenerator": true,
         "useESModules": false
